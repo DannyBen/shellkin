0ea135d8a0618dd40ba64b038ef63e50592f056f
Danny Ben Shitrit <db@dannyben.com>
2026-03-22 18:02:04 +0200

    normalize fixture stepdefs

--- features/fixtures/configurable/steps/core.sh
+++ features/fixtures/configurable/steps/core.sh
@@ -1,8 +1,8 @@
 @When I print configured output
-run 'printf configured'
+  run 'printf configured'
 
 @When I print loaded support values
-run "printf '%s' \"$SUPPORT_VALUE\""
+  run "printf '%s' \"$SUPPORT_VALUE\""
 
 @Then the output should include '{text}'
-[[ "$LAST_STDOUT" == *"$text"* ]]
+  [[ "$LAST_STDOUT" == *"$text"* ]]
